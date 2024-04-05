8b559f669cfc5d8cc531011ef6c53382105f5684
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-05 13:19:15 +0100

    Customise admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('email', 'password', )}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('user_info'), {'fields': ('phone_number',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff', 'phone_number']
+    search_fields = ('email', 'first_name', 'last_name', 'phone_number')
+    ordering = ('last_name', 'email', )
+
+admin.site.register(User, UserAdmin)
+
