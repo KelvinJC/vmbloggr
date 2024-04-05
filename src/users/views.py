2a81a3e88e158ce7706a1e96324286214cb5cf44
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-05 13:46:58 +0100

    Define user views

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import generics, status, permissions, viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import User
+from .serializers import LoginSerializer, UserSerializer, UserCreateSerializer
+from .permissions import IsUserOwner
+from . import utils 
+
+
+class UserCreateView(generics.GenericAPIView):
+    serializer_class = UserCreateSerializer
+
+    def post(self, request):
+        user = request.data
+        serializer = self.get_serializer(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class UserViewSet(utils.ListRetrieveUpdateDestroyViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [IsAuthenticated, IsUserOwner]
+
+    def get_permissions(self):
+        if self.action in ('destroy', 'update', 'partial_update') :
+            permission_classes = [IsAuthenticated, IsUserOwner]
+        else:
+            permission_classes = [permissions.AllowAny]  # For other actions
+
+        return [permission() for permission in permission_classes]
+       
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.email != request.user.email:
+            return Response(
+                {"detail": "You don't have permission to delete this account."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class LoginAPIView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
