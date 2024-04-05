55062c3cb9c11421f3a3976b860dc39fa8ba61c4
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-05 11:51:22 +0100

    Add routes for docs

--- src/vmbloggr/urls.py
+++ src/vmbloggr/urls.py
@@ -15,8 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="vmbloggr API",
+        default_version='v0.0.1',
+        description="An API for a simple blog app",
+        terms_of_service="https://www.kaizen_vmbloggr.com/terms/",
+        contact=openapi.Contact(email="ojkelvinchu@gmail.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    # documentation
+    re_path(r'^swagger\.(?P<format>(json|yaml))$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
