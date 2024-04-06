8c4db9d8bb1b95230d265f929173324b3574437e
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-06 16:39:51 +0100

    Add script to programmatically create superuser

---
+++ src/create_superuser.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+if not UserModel.objects.filter(username='Admin').exists():
+    user = UserModel.objects.create(username='Admin', password='Admin')
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
