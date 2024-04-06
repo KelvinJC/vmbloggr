db943e60ad5e2d6a1ecf59b835bd521a6bfdc4b8
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-06 09:18:48 +0100

    Define and register model admin

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subtitle', 'author', 'date_created', 'is_deleted']
+    search_fields = ['title', 'subtitle', 'body']
+    list_filter = ['author', 'date_created', 'is_deleted']
+    date_hierarchy = 'date_created'
+    readonly_fields = ['date_created']
