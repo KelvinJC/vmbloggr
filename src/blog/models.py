a6f9c11a9bb9676ace4a194451099711060ac959
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-06 09:17:41 +0100

    Rename attributes, manager methods

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
 
+
 class BlogPostModelManager(models.Manager):
     
     def delete(self, id):
@@ -13,25 +14,24 @@ class BlogPostModelManager(models.Manager):
     def get_all(self):
         ''' get all blog posts'''
         query = self.get_queryset()
-        return query.filter(blogpost__is_deleted=False)
+        return query.filter(is_deleted=False)
     
-    def get(self, id):
+    def find(self, id):
         ''' get a single blog post '''
         return self.get(pk=id)
     
     def find_by_author(self, author_id):
         ''' search for all blog posts by a common author'''
-        pass
+        return self.get_queryset().filter(author=author_id)
 
     
 
 class BlogPost(models.Model):
-
-    title = models.TextField(null=True, blank=True)
-    subTitle = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='author')
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
     body = models.TextField(null=True, blank=True)
-    dateCreated = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='author')
+    date_created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
 
     objects = BlogPostModelManager()
