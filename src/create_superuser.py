2ce661ec7a61c21e25d9d02b5c9ed0e417b46d9a
Kelvin <kelvin.johnson-chu@abujaelectricity.com>
2024-04-16 16:34:30 +0100

    Remove if statement to ensure admin user is created

--- src/create_superuser.py
+++ src/create_superuser.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
-if not UserModel.objects.filter(username='Admin').exists():
-    user = UserModel.objects.create_user('Admin', password='Admin12')
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
+user = UserModel.objects.create_user('Admin', password='Admin')
+user.is_superuser = True
+user.is_staff = True
+user.save()
